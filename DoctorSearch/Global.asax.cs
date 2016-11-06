f28b57b2e280eee4601bc155e5930771fc779572
chuckwang123 <wangjohn419@gmail.com>
2016-11-06 09:50:02 -0600

    Add Serilog

--- DoctorSearch/Global.asax.cs
+++ DoctorSearch/Global.asax.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
+using Amazon.CloudWatchLogs;
+using Amazon.Runtime;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
 
 namespace DoctorSearch
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start(IHostingEnvironment env)
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var logGroupName = "Logger/" + env.EnvironmentName;
+
+            // customer renderer (optional, defaults to a simple rendered message of Serilog's LogEvent
+            var renderer = new CustomRenderer();
+
+            // options for the sink, specifying the log group name
+            var options = new CloudWatchSinkOptions { LogGroupName = logGroupName, LogEventRenderer = renderer };
+
+            // setup AWS CloudWatch client
+            var credentials = new BasicAWSCredentials(myAwsAccessKey, myAwsSecretKey);
+            var client = new AmazonCloudWatchLogsClient(credentials, myAwsRegion);
+
+            // Attach the sink to the logger configuration
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+              .WriteTo.AmazonCloudWatch(options, client)
+              .CreateLogger();
         }
     }
 }
